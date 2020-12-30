154e3f9783aae4a77191bef197c44433d0ebd2ea
Silvino <silvino.rodriguez@globant.com>
2020-12-30 09:50:24 -0600

    Sidebar feature: The Sidebar view has been finished

--- src/ux/sidebar/index.js
+++ src/ux/sidebar/index.js
@@ -6,52 +6,50 @@ export const Sidebar = () => (
     <div className="sidebar">
         <div className="sidebar-options">
             <div className="filters">
-                <div>
-                    <p>Filters</p>
+
+                <p className='filters-title'>Filters</p>
+                <div className='filters-container'>
                     <div>
-                        <div>
-                            <input type="checkbox" id="basics" name="basics"
-                                checked />
-                            <label for="vasics">Basics</label>
-                        </div>
+                        <input type="checkbox" id="basics" name="basics"
+                            checked />
+                        <label for="vasics">Basics</label>
                     </div>
                 </div>
+
             </div>
             <div className="prices">
-                <div>
-                    <p>Prices</p>
+                <p className='prices-title'>Prices</p>
+                <div className='prices-container'>
                     <div>
-                        <div>
-                            <input type="radio" id="huey" name="drone" value="huey"
-                                checked />
-                            <label for="huey">$1 - $50</label>
-                        </div>
-                        <div>
-                            <input type="radio" id="huey" name="drone" value="huey"
-                                checked />
-                            <label for="huey">$51 - $100</label>
-                        </div>
-                        <div>
-                            <input type="radio" id="huey" name="drone" value="huey"
-                                checked />
-                            <label for="huey">$101 - $200</label>
-                        </div>
+                        <input type="radio" id="huey" name="drone" value="huey"
+                            checked />
+                        <label for="huey">$1 - $50</label>
+                    </div>
+                    <div>
+                        <input type="radio" id="huey" name="drone" value="huey"
+                            checked />
+                        <label for="huey">$51 - $100</label>
+                    </div>
+                    <div>
+                        <input type="radio" id="huey" name="drone" value="huey"
+                            checked />
+                        <label for="huey">$101 - $200</label>
                     </div>
                 </div>
+
             </div>
             <div className="sortby">
-                <div>
-                    <p>Sort By</p>
-                    <div>
 
-                        <select id="cars" name="Relevant" form="carform">
-                            <option value="Relevant">Relevant</option>
-                            <option value="saab">Saab</option>
-                            <option value="opel">Opel</option>
-                            <option value="audi">Audi</option>
-                        </select>
-                    </div>
+                <p>Sort By</p>
+                <div>
+                    <select id="cars" name="Relevant" form="carform">
+                        <option value="Relevant">Relevant</option>
+                        <option value="saab">Saab</option>
+                        <option value="opel">Opel</option>
+                        <option value="audi">Audi</option>
+                    </select>
                 </div>
+
             </div>
         </div>
     </div>
