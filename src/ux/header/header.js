a035b876ae2d8c722cf45e3b0278e6535c583714
Silvino <silvino.rodriguez@globant.com>
2021-01-04 17:34:54 -0600

    Header feature: The component design has been modified

--- src/ux/header/header.js
+++ src/ux/header/header.js
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types';
 import shopping_cart from '../../assets/cart.svg'
 import globant_shops from '../../assets/globant-shops.svg'
 
-export const Header = ({ user }) => (
+export const Header = ({ handleIsOpenSidebar }) => (
 	<header>
 		<div className="header bg-primary d-flex justify-content-between">
 			<div className='globant-shops'>
-				<img src={globant_shops} />
+				<img src={globant_shops} onClick={handleIsOpenSidebar} />
 			</div>
 
 			<div className='shopping-cart'>
