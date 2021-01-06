cf4f4d69bce51109c8240d038017817a18915e64
Silvino <silvino.rodriguez@globant.com>
2021-01-05 18:58:26 -0600

    OurProducts view has been modified

--- src/ux/views/our_products/index.js
+++ src/ux/views/our_products/index.js
@@ -2,21 +2,16 @@ import React from 'react';
 
 import { ProductItem } from '../../product_item';
 
-export const OurProducts = () => <div >
+export const OurProducts = ({ list }) => <div >
     <div className='our-products'>
         <div className='our-products-title'>
             <p>Our products:</p>
         </div>
-        <div className='d-flex justify-content-between'>
-            <ProductItem />
-            <ProductItem />
-            <ProductItem />
+        <div className='d-flex flex-wrap'>
+            {list.map(item => <ProductItem {...item} />)}
+
         </div>
-        <div className='d-flex justify-content-between'>
-            <ProductItem />
-            <ProductItem />
-            <ProductItem />
-        </div>
+
     </div>
 </div>;
 
