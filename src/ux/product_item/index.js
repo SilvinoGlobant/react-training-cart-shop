aba0f7382b8b093345892c20b1fbdcfa870ee605
Silvino <silvino.rodriguez@globant.com>
2021-01-05 19:04:17 -0600

    ProductItem feature: Props have been added to the component

--- src/ux/product_item/index.js
+++ src/ux/product_item/index.js
@@ -1,33 +1,30 @@
 import React from 'react';
-import coffe from '../../assets/coffe.svg'
-import star_rate from '../../assets/star-rate.svg'
-import comment from '../../assets/comment.png'
-export const ProductItem = () => {
-    return <>
-        <div className="product-item d-flex flex-column">
-            <span className='badge bg-third d-flex justify-content-center'>BASIC</span>
-            <img className='img-item' src={coffe} alt="" />
-            <p className='title-item'>Coffe Maker</p>
-            <div className='rate-container d-flex'>
-                <div className='start-rate-container'>
-                    <img src={star_rate} alt="" />
-                    <img src={star_rate} alt="" />
-                    <img src={star_rate} alt="" />
-                    <img src={star_rate} alt="" />
-                    <img src={star_rate} alt="" />
-                </div>
-                <div className='comment-container'>
-                    <span>2</span>
-                    <img src={comment} alt="" />
-                </div>
+import coffe from '../../assets/coffe.svg';
+import star_rate from '../../assets/star-rate.svg';
+import comment from '../../assets/comment.png';
+
+export function ProductItem({ id, name, rate, price, comments, basics, img }) {
+    return <div className="product-item d-flex flex-column">
+        {basics ? <span className='badge bg-third d-flex justify-content-center'>BASIC</span> : null}
+        <img className='img-item' src={img || coffe} alt="" />
+        <p className='title-item'>{name || 'No name'}</p>
+        <div className='rate-container d-flex'>
+            <div className='start-rate-container'>
+                <img src={star_rate} alt="" />
+                <img src={star_rate} alt="" />
+                <img src={star_rate} alt="" />
+                <img src={star_rate} alt="" />
+                <img src={star_rate} alt="" />
             </div>
-            <p className='item-amount'>$105</p>
-            <div className='btn-container d-flex flex-column'>
-                <button className='bg-primary'>See details</button>
-                <button className='bg-secondary'> Add to cart</button>
+            <div className='comment-container'>
+                <span>{comments || 0}</span>
+                <img src={comment} alt="" />
             </div>
         </div>
-
-    </>
-
+        <p className='item-amount'>${price || '0.00'}</p>
+        <div className='btn-container d-flex flex-column'>
+            <button className='bg-primary'>See details</button>
+            <button className='bg-secondary'> Add to cart</button>
+        </div>
+    </div>
 }
