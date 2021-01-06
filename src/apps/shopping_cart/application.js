bf47e6527665e8a9b4a21cf59cfb5fa68b4734c1
Silvino <silvino.rodriguez@globant.com>
2021-01-06 12:54:46 -0600

    ShoppingCart app feature: Filters for the app has been added

--- src/apps/shopping_cart/application.js
+++ src/apps/shopping_cart/application.js
@@ -5,18 +5,20 @@ import { productsActions } from './actions';
 import { currentListSelector } from './selectors';
 import Layout from './components/layout';
 import MainPage from './components/main_page'
-// import { productsActions } from '../shopping_cart/actions';
-
 
 function Application({ getProducts, productList }) {
 
     const navigate = useNavigate();
 
     const [productListA, setProductListA] = useState([]);
+    const [isBasic, setIsBasic] = useState(false);
+    const [radioButtonActive, setRadioButtonActive] = useState(1);
 
     const handleFilterList = (event) => {
 
         const isBasics = event.target.checked;
+
+        setIsBasic(isBasics);
 
         if (isBasics) {
             const filteredList = productListA.filter(item => item.basics);
@@ -24,7 +26,34 @@ function Application({ getProducts, productList }) {
             return;
         }
 
+        setRadioButtonActive(0);
         setProductListA(productList.records);
+
+    }
+
+    const handlePricesOptions = (item) => {
+
+        if (isBasic) {
+            const filteredList = productList.records
+                .filter(item => item.basics)
+                .filter(element => {
+                    if (item === 1 && (element.price >= 1 && element.price <= 50)) return item
+                    if (item === 2 && (element.price >= 51 && element.price <= 100)) return item
+                    if (item === 3 && (element.price >= 101 && element.price <= 200)) return item
+
+                });
+
+            setProductListA(filteredList);
+            return;
+        }
+
+        const filteredList = productList.records.filter(element => {
+            if (item === 1 && (element.price >= 1 && element.price <= 50)) return item
+            if (item === 2 && (element.price >= 51 && element.price <= 100)) return item
+            if (item === 3 && (element.price >= 101 && element.price <= 200)) return item
+        });
+
+        setProductListA(filteredList);
 
     }
 
@@ -42,7 +71,14 @@ function Application({ getProducts, productList }) {
     }, [productList])
 
     return <Routes>
-        <Route path='/' element={<Layout handleFilterList={handleFilterList} />}>
+        <Route path='/'
+            element={<Layout
+                handleFilterList={handleFilterList}
+                handlePricesOptions={handlePricesOptions}
+                radioButtonActive={radioButtonActive}
+                setRadioButtonActive={setRadioButtonActive}
+
+            />}>
             <Route path='shopping' element={<MainPage productList={productListA} />} />
         </Route>
     </Routes>
