5abb09a62e933284e18d38f528711659bf5dbdc1
Silvino <silvino.rodriguez@globant.com>
2021-01-08 13:06:07 -0600

    PaymentSuccess view storie feature: The view storie was created

---
+++ src/ux/stories/views/payment_success/payment_success.stories.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import success from '../../../../assets/success1.webp';
+
+export default {
+    title: 'Views/PaymentSuccess',
+    // component: OurProducts,
+    parameters: {
+        position: "1.0"
+    }
+};
+
+const PaymentSuccess = () => (
+    <div className='payment-success w-100 d-flex flex-column justify-content-center align-items-center'>
+        <img src={success} alt="" />
+        <h3>Congratulations! your order has shipped</h3>
+    </div>
+)
+
+export const PaymentSuccessDemo = PaymentSuccess.bind({});
