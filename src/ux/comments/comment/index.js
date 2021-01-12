b33017822bc8fef478597fa468ff1973a5f7dfc0
Silvino <silvino.rodriguez@globant.com>
2021-01-12 12:17:38 -0600

    Comment component feature: The props were added

--- src/ux/comments/comment/index.js
+++ src/ux/comments/comment/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 
-
-export default function Comment() {
+export default function Comment({ user, comment }) {
     return (
         <div className='comment'>
-            <p className='comment-user'>Jon Doe</p>
-            <p className='comment-text'>Excelent Product </p>
+            <p className='comment-user'>{user}</p>
+            <p className='comment-text'>{comment} </p>
         </div>
     );
 }
