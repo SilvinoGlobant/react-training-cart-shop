ed881970614b3707f7091c96dc740292a459e15a
Silvino <silvino.rodriguez@globant.com>
2021-01-12 11:32:32 -0600

    ProductDetails proxy feature: The proxy has been created

--- src/apps/shopping_cart/components/product_details/index.js
+++ src/apps/shopping_cart/components/product_details/index.js
@@ -1,24 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { productActions } from '../../actions';
-import { DetailsDemo } from 'ux/stories/views/details/details.stories';
+import ProductDetails from 'ux/views/details';
+import { currentProductSelector } from 'apps/shopping_cart/selectors';
 
-function ProductsDetailProxy({ getProductsDetail }) {
+function ProductsDetailProxy({ getProductsDetail, currentProduct }) {
 
     const { productId } = useParams();
+    const [product, setProduct] = useState({});
 
     useEffect(() => {
         if (!productId) return;
         getProductsDetail(productId)
     }, [productId, getProductsDetail])
 
-    return <DetailsDemo />
+    useEffect(() => {
+        if (!currentProduct || currentProduct.isLoading) return;
+        setProduct(currentProduct.record)
+    }, [currentProduct])
 
+    return <ProductDetails product={product} />
 
 }
 export default connect(
-    state => ({}),
+    state => ({
+        currentProduct: currentProductSelector(state)
+    }),
     dispatch => ({
         getProductsDetail: (productId) => dispatch(productActions.getDetails(productId))
     })
