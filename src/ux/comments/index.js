b29d1a96c4f9cfcb78a7b61165432a371c902ddf
Silvino <silvino.rodriguez@globant.com>
2021-01-12 12:16:59 -0600

    Comments component feature: The props were added

--- src/ux/comments/index.js
+++ src/ux/comments/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import Comment from './comment'
 
-export default function Comments() {
+export default function Comments({ comments }) {
     return <div className='comments'>
         <p className='comments-title'>Comments:</p>
         <div className='comments-container'>
-            <Comment />
-            <Comment />
-            <Comment />
-            <Comment />
+            {comments?.map((item, index) => <Comment key={`comment-${index}`} {...item} />)}
         </div>
     </div>
 
