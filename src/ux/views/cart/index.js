df68e47bb92025d0ffe6516926a99c946f8c497c
Silvino <silvino.rodriguez@globant.com>
2021-01-13 12:40:20 -0600

    Cart view component feature: The cart view componend has been created

---
+++ src/ux/views/cart/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CartDemo from '../../cart';
+
+export default function Cart({ handleCloseCartView, navigateToPayment }) {
+    return (
+        <div className='cart-view d-flex w-100'>
+            <div className='cart-view-black w-100' />
+            <CartDemo
+                navigateToPayment={navigateToPayment}
+                handleCloseCartView={handleCloseCartView} />
+        </div>
+    );
+}
